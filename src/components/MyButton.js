1c306de923f44cd62c1f667824c030be1be61c51
salvadorsuz <salvadorsuz@yahoo.es>
2019-06-10 16:20:53 +0200

    fix prettier

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 
 type Params = {
   name?: string,
   countFunc: (count: number) => void
 };
 
-const MyButton = ({ name='Click', countFunc }: Params) => (
-  <button type="button" onClick={() => countFunc(1)}>{name}</button>
+const MyButton = ({ name = "Click", countFunc }: Params) => (
+  <button type="button" onClick={() => countFunc(1)}>
+    {name}
+  </button>
 );
 
 export default MyButton;
